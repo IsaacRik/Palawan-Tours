7c0bb8d7d52c3968d81aee9ad75a5bdc9d7c1c76
isaac rikmanis <isaac.rikmanis@gmail.com>
2024-12-21 10:54:01 +0800

    added sited

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Palawan Tours",
+    "email": "Amihanhostel@gmail.com",
+    "phoneForTel": "NA",
+    "phoneFormatted": "(555) 779-4407",
+    "address": {
+        "lineOne": "First Address Line",
+        "lineTwo": "Second Address Line",
+        "city": "Poblacion",
+        "state": "San Vicente",
+        "zip": "NA",
+        "country": "Phillipines",
+        "mapLink": "https://maps.app.goo.gl/eiQURrmMQcj1hzJu8"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.example.com"
+};
