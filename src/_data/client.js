85aeea0cde0f3563f9fec1e01cb95e8081d09f1d
isaac rikmanis <isaac.rikmanis@gmail.com>
2025-01-02 15:50:58 +0800

    g

--- src/_data/client.js
+++ src/_data/client.js
@@ -2,20 +2,20 @@ module.exports = {
     "name": "Palawan Tours",
     "email": "Amihanhostel@gmail.com",
     "phoneForTel": "NA",
-    "phoneFormatted": "(555) 779-4407",
+    "phoneFormatted": "+63 998 497 9135",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Poblacion",
-        "state": "San Vicente",
-        "zip": "NA",
+        "lineOne": "Sitio penagmgalokan",
+        "lineTwo": "Longbeach",
+        "city": "San Vicente",
+        "state": "Palawan",
+        "zip": "5309",
         "country": "Phillipines",
         "mapLink": "https://maps.app.goo.gl/eiQURrmMQcj1hzJu8"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
+        "instagram": "https://www.instagram.com/amihanbeachhostel/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.palawan.tours"
 };
